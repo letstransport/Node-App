c429829dda4e10f5e9315acdbba7e21ff24c6432
letstransport <tech@letstransport.in>
2019-11-17 19:24:17 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello Node!\n');
+  res.end('Hello Node, welcome to mayank ec2!\n');
 });
 
 server.listen(port, () => {
